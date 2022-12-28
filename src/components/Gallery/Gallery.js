cdb3f1c95e1b668e89d9a19a105cd81b3c857a31
blayres <barbara.ayres.pro@gmail.com>
2022-12-28 12:40:12 +0100

    indicators slide ok

--- src/components/Gallery/Gallery.js
+++ src/components/Gallery/Gallery.js
@@ -5,25 +5,38 @@ import ArrowBack from '../Icons/ArrowBack'
 import ArrowForward from '../Icons/ArrowForward'
 
 function Gallery({ pictures }) {
-    const [index, setIndex] = useState(0)
-    const next = () => {
-        if (index < pictures.length - 1) {
-            setIndex(index + 1)
-        } else { setIndex(0) }
+
+    const [currentSlide, setCurrentSlide] = useState(0);
+
+    const goToNextSlide = () => {
+        if (currentSlide === pictures.length - 1) {
+          setCurrentSlide(0);
+        } else {
+          setCurrentSlide(currentSlide + 1);
+        }
+      }
+
+    const goToPreviousSlide = () => {
+        if (currentSlide === 0) {
+        setCurrentSlide(pictures.length - 1);
+        } else {
+        setCurrentSlide(currentSlide - 1);
+        }
     }
-    const prev = () => {
-        if (index > 0) {
-            setIndex(index - 1)
-        } else { setIndex(pictures.length - 1) }
-    }
+
     return(
         <div className='gallery-slider'>
+            {pictures.map((picture, index) => (
             <div className='gallery-slider-btns'>
-                <div className='arrow' onClick={prev}> <ArrowBack /></div>
-                <div className='arrow' onClick={next}> <ArrowForward /></div>
+                <div key={picture.id} className='arrow' onClick={goToPreviousSlide}> <ArrowBack /></div>
+                <div key={picture.id} className={index === currentSlide ? 'active' : ''}  onClick={goToNextSlide}> <ArrowForward /> </div>
             </div>
+                ))}
 
-            <img src={pictures[index]} alt='' />
+            <img src={pictures[currentSlide]} alt='' />
+            <div className='indicators'>
+                {currentSlide + 1}/{pictures.length}
+            </div>
         </div>
     )
 }
